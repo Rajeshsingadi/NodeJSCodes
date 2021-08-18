c27daaa667c71713c7d11f5051905f3c6965511e
Rajesh <rajeshkumarraje9@gmail.com>
2021-08-18 21:50:18 +0530

    os module updated

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var logger = require('./logger');
-logger("message Displayed using the variables");
+var logger = require('./logger')
+logger("message Displayed using the variables")
 
-console.log("This is a demonstraion on the path module");
-const path = require('path');
-var pathobj = path.parse(__filename);
-console.log(pathobj);
+console.log("This is a demonstraion on the path module")
+const path = require('path')
+var pathobj = path.parse(__filename)
+console.log(pathobj)
 
 console.log("Demonstration on os module")
 const os = require('os')
--- logger.js
+++ logger.js
@@ -1,10 +1,10 @@
-console.log(__filename);
-console.log(__dirname);
+console.log(__filename)
+console.log(__dirname)
 
-var url = 'http://logger.io/log';
+var url = 'http://logger.io/log'
 
 function log(message) {
-    console.log(message);
+    console.log(message)
 }
 
-module.exports = log;
+module.exports = log
