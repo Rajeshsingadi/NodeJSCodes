156fa6b36eec6717c8704d9a1005fccd72261e89
Rajesh <rajeshkumarraje9@gmail.com>
2021-08-18 21:35:31 +0530

    path module concept

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var logger = require('./logger');
-logger("message Displayed using the variables")
+logger("message Displayed using the variables");
 
+console.log("This is a demonstraion on the path module")
+const path = require('path');
+var pathobj = path.parse(__filename);
+console.log(pathobj)
